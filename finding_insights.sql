ec4081c241e161fcd5f68a54e2cce2a8c0c61ea9
DylRey18 <111478803+DylRey18@users.noreply.github.com>
2024-12-30 14:52:14 +0700

    Uploading initial Relational Diagram

---
+++ finding_insights.sql
@@ -0,0 +1,22 @@
+USE crypto; 
+
+#Checking what each table contains
+select * from cryptocurrency;
+
+select * from transaction;
+
+select * from user;
+
+select * from user_wallet;
+
+#Start of exploratory
+## Analytics Queries
+#1. **Total Value of Cryptocurrencies in User Wallets**: Calculates the total value of cryptocurrencies held by each user.
+
+#2. **Most Traded Cryptocurrency**: Finds the cryptocurrency with the highest number of transactions.
+
+#3. **Users with the Highest Number of Transactions**: Identifies users who have made the most transactions.
+
+#4. **Pending Transactions**: Lists all transactions that are currently pending.
+
+#5. **Average Transaction Amount per Cryptocurrency**: Calculates the average transaction amount for each cryptocurrency.
